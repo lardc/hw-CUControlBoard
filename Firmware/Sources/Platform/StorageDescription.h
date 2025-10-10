9b44e44f6704d1fa638f2b424a92701004a05841
Alexander-Mamasaklisov <a.mamasakhlisov@proton-electrotex.com>
2025-10-10 12:27:24 +0300

    Библиотеки SaveToFlash взяты из TMS вместо встроенных в прошивку; добавлен SaveToFlashConfig.h; в StorageDescription добавлены реле из таблиц коммутации

--- Firmware/Sources/Platform/StorageDescription.h
+++ Firmware/Sources/Platform/StorageDescription.h
@@ -1,4 +1,4 @@
-﻿#ifndef __STORAGE_DESCRIPTION_H
+#ifndef __STORAGE_DESCRIPTION_H
 #define __STORAGE_DESCRIPTION_H
 
 // Include
@@ -6,8 +6,25 @@
 #include "SaveToFlash.h"
 
 // Variables
-extern const RecordDescription StorageDescription[];
+extern RecordDescription StorageDescription[];
 extern Int32U TablePointers[];
 extern const Int16U StorageSize;
+
+extern CounterDescription *CounterStorageDescription;
+extern CounterData *CounterTablePointers;
+extern Int16U CounterStorageSize;
+
+extern CounterDescription CounterStorageDescription2[];
+extern CounterDescription CounterStorageDescription4[];
+extern CounterDescription CounterStorageDescription6[];
+extern CounterDescription CounterStorageDescription6Gate4Wire[];
+extern CounterData CounterTablePointers2[];
+extern CounterData CounterTablePointers4[];
+extern CounterData CounterTablePointers6[];
+extern CounterData CounterTablePointers6Gate4Wire[];
+extern const Int16U CounterStorageSize2;
+extern const Int16U CounterStorageSize4;
+extern const Int16U CounterStorageSize6;
+extern const Int16U CounterStorageSize6Gate4Wire;
 
 #endif // __STORAGE_DESCRIPTION_H
